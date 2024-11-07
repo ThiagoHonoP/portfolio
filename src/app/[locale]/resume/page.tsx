ca7a58a598d5b01fcd91bdd6f3cd863c47ee6ecd
ThiagoHonoP <thiagopedrosa.thp@gmail.com>
2024-11-07 07:49:49 -0300

    chore: add utils folder

---
+++ src/app/[locale]/resume/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import FooterLink from "@/components/Footer";
+import { Rocket } from "@/components/Icons";
+import Layout, { PageWrapper } from "@/components/Layout";
+import { PageSection } from "@/components/Layout/styles";
+import { Typography } from "@/components/Typography";
+import useTranslation from "@/hooks/useTranslation";
+import { AboutSection } from "@/styles/pages/about/styles";
+import Link from "next/link";
+
+export default function Resume() {
+  const { t, locale } = useTranslation();
+
+  return (
+    <Layout>
+      <PageSection>
+        <PageWrapper>
+          <h1 data-text="Thiago Honorato dev" className="intro__text">
+            RESUME
+          </h1>
+          <article>
+            <br />
+            <FooterLink goto={`/${locale}/contact`}>
+              {t("homeFooter")}
+            </FooterLink>
+          </article>
+        </PageWrapper>
+      </PageSection>
+    </Layout>
+  );
+}
