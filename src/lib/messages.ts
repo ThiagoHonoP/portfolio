cca783fec3dd7faf822e482c3bc222ce5c242060
ThiagoHonoP <thiagopedrosa.thp@gmail.com>
2024-11-07 07:47:54 -0300

    chore: add  messages translate

---
+++ src/lib/messages.ts
@@ -0,0 +1,78 @@
+interface LangStringsTypes {
+  [key: string]: string;
+  home: string;
+  homeDescription: string;
+  homeFooter: string;
+  about: string;
+  aboutFullStack: string;
+  aboutFrontEnd: string;
+  aboutWebDesigner: string;
+  aboutFooter: string;
+  projects: string;
+  projectsFooter: string;
+  contact: string;
+  contactText: string;
+  labelName: string;
+  labelMessage: string;
+  buttonText: string;
+  aboutText: string;
+  slogan: string;
+}
+
+export const LangStrings: { [key: string]: LangStringsTypes } = {
+  en: {
+    home: "Home",
+    homeDescription:
+      "I am a Full stack Javascript Developer with a passion for creating digital experiences that captivate and inspire. With a strong background in front-end development and graphic design, I blend technical skills with an eye for aesthetics to craft applications that exceed user expectations. I stay up-to-date with the latest technologies, enabling innovative and visually engaging solutions. My goal is to develop scalable and robust solutions that drive client business growth, resulting in successful projects.",
+    homeFooter: "See More About Me",
+    about: "About",
+    aboutFullStack:
+      "I am currently focused on expanding my knowledge of software architecture and cloud computing. Enthusiastic about the challenges that this journey brings, I am committed to leveraging my prior experience to learn and grow as a Fullstack developer.",
+    aboutFrontEnd:
+      "As a Frontend developer, I assisted in the development of applications focused on web3, management dashboards, and web extensions, creating a positive impact for users consuming content on cryptoeconomics, blockchain, and web3. My responsibilities included developing new features, implementing a Design System, integrating APIs, creating components, and conducting code reviews. In addition, I actively participated in mobile projects.",
+    aboutWebDesigner:
+      "As a web designer, I've had the privilege of collaborating with a variety of clients and companies, ranging from digital agencies to e-commerce platforms and printing companies. These experiences have allowed me to enhance my skills in creating institutional websites, landing pages, visual identity, and printed corporate materials, as well as providing valuable insights into the diverse needs of each client.",
+    aboutFooter: "Lets Continue To Projects",
+    technologies: "Technologies",
+    projects: "Projects",
+    projectsFooter: "Let's Go To My Resume",
+    contact: "Contact",
+    contactText: "Get in touch or shoot me an email directly on ",
+    contactFooter: "Go Back Home",
+    labelName: "Name",
+    labelMessage: "Message",
+    buttonText: "Send Message",
+    aboutText:
+      "Site feito para mostrar a criação de um site bilíngue utilizando o Next.js. O tutorial está em um artigo do meu blog. Fique a vontade para ver o código-fonte, fazer um fork, ou até usá-lo em seus projetos.",
+    slogan: "An example site showcasing a bilingual site with Next.js.",
+    footerText: "See more about",
+  },
+  pt: {
+    home: "Home",
+    homeDescription:
+      "Sou um desenvolvedor Full stack Javascript apaixonado por criar experiências digitais que cativam e inspiram. Com uma sólida experiência em desenvolvimento front-end e design gráfico, combino habilidades técnicas com um olhar para a estética para criar aplicativos que excedem as expectativas do usuário. Fico atualizado com as últimas tecnologias, permitindo soluções inovadoras e visualmente envolventes. Meu objetivo é desenvolver soluções escaláveis ​​e robustas que impulsionem o crescimento dos negócios do cliente, resultando em projetos bem-sucedidos.",
+    homeFooter: "Veja Mais Sobre Mim",
+    about: "Sobre",
+    aboutFullStack:
+      "Atualmente estou focado em expandir meu conhecimento sobre arquitetura de software e cloud compunting. Empolgado com os desafios que essa jornada proporciona, estou comprometido em aplicar minha experiência prévia para aprender e crescer como desenvolvedor Fullstack.",
+    aboutFrontEnd:
+      "Como Frontend auxiliei no desenvolvimento de Aplicacões focadas em web3, paineis de gerenciamento e extensoes web gerando impacto positivo para os usuairos que consomem conteudos sobre criptoeconomia, blockchain e web3. As minhas responsabilidades incluíam desenvolvimento de novas features, implementação de Design System, integrar APIs, criação de components, code review. Além da atuação ativa em projetos mobile.",
+    aboutWebDesigner:
+      "Atuando como designer web, tive o privilégio de colaborar com uma variedade de clientes e empresas , abrangendo desde agências digitais até e-commerces e gráficas. Essas experiências me permitiram aprimorar minhas habilidades em criar sites institucionais, landing pages, identidade visual e materiais corporativos impressos, além de me proporcionar insights valiosos sobre as necessidades de cada cliente.",
+    aboutFooter: "Veja alguns projetos",
+    technologies: "Tecnologias",
+    projects: "Projetos",
+    projectsFooter: "Veja meu currículo",
+    contact: "Contato",
+    contactText: "Entre em contato ou envie-me um e-mail diretamente para ",
+    contactFooter: "Voltar a home",
+    labelName: "Nome",
+    labelMessage: "Mensagem",
+    buttonText: "Enviar Mensagem",
+    aboutText:
+      "Site made to showcase the creation of a bilingual website using Next.js. The tutorial is in an article on my blog. Feel free to view the source code, fork it, or even use it in your projects.",
+    articles: "Artigos",
+    slogan: "Um site bilíngue de exemplo feito com Next.js. 123123123123123",
+    footerText: "Veja mais sobre",
+  },
+};
